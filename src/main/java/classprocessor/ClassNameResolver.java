eafcd35e8ee41826aed584c3170ebedd4770f296
Tsutomu YANO <t_yano@me.com>
2015-05-28 09:51:49 +0900

    Big change of package-name. shelfmap.classprocessor -> classprocessor.

---
+++ src/main/java/classprocessor/ClassNameResolver.java
@@ -0,0 +1,10 @@
+package classprocessor;
+
+/**
+ * An interface for the classes which generate a simple class name of an interface.
+ * @author Tsutomu YANO
+ */
+public interface ClassNameResolver {
+    String classNameFor(String className);
+    String abstractClassNameFor(String className);
+}
